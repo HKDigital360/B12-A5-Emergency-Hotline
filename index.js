32cf15eae4021109c101712b9f655725b33d3243
HKDigital360 <hkibria528@gmail.com>
2026-01-10 00:10:35 +0600

    resposiveness almost done

--- index.js
+++ index.js
@@ -39,3 +39,37 @@ const total = availableRedHeartNum + 1
 
 
 
+const callButton = document.querySelectorAll(".btn-call-action")
+
+callButton.forEach(function(button){
+    button.addEventListener("click", function(){
+
+    //   const emergencyElement = document.getElementsByClassName('emergency')[0]; 
+    //   console.log(emergencyElement.innerText);
+
+    const allEmergencyElements = document.querySelectorAll('.emergency'); allEmergencyElements.forEach(function(el) {  
+        alert(el.textContent);
+        return;
+    });
+
+    // console.log(el.textContent);
+
+
+        
+        
+
+const availableCoins = parseInt(document.getElementById("btn-100-cions").innerText)
+
+
+const totalCoins = availableCoins - 20
+
+document.getElementById("btn-100-cions").innerText = totalCoins
+
+ 
+
+
+    })
+
+    
+})
+
